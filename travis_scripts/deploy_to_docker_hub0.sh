8343f3889303a8417294ea7c1646fdf26409a453
Young Jun Park <june0313@gmail.com>
2019-07-22 23:34:56 +0900

    change docker hub account

--- travis_scripts/deploy_to_docker_hub0.sh
+++ travis_scripts/deploy_to_docker_hub0.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 #docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push dockerever/tmx-authentication-service:$BUILD_NAME
-docker push dockerever/tmx-licensing-service:$BUILD_NAME
-docker push dockerever/tmx-organization-service:$BUILD_NAME
-docker push dockerever/tmx-confsvr:$BUILD_NAME
-docker push dockerever/tmx-eurekasvr:$BUILD_NAME
-docker push dockerever/tmx-zuulsvr:$BUILD_NAME
+docker push june0313/tmx-authentication-service:$BUILD_NAME
+docker push june0313/tmx-licensing-service:$BUILD_NAME
+docker push june0313/tmx-organization-service:$BUILD_NAME
+docker push june0313/tmx-confsvr:$BUILD_NAME
+docker push june0313/tmx-eurekasvr:$BUILD_NAME
+docker push june0313/tmx-zuulsvr:$BUILD_NAME
